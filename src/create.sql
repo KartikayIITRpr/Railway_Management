917222e5ca589fab885bb43170265853c8aac957
Kartikay Singh <kbsingh296@gmail.com>
2022-11-15 10:12:52 +0530

    Updated create queries

--- src/create.sql
+++ src/create.sql
@@ -1,26 +1,19 @@
-create table train
-(
-  train_num INT NOT NULL,
-  train_name varchar(50),
-  primary key (train_num)
-);
-
 create table schedule(
   train_num INT NOT NULL,
   running_on DATE NOT NULL,
   num_ac INT NOT NULL,
   num_sleeper INT NOT NULL,
-  primary key (train_num,running_on),
-  foreign key (train_num) references train(train_num)
+  primary key (train_num,running_on)
 );
 
 create table ticket(
  pnr serial NOT NULL,
  train_num INT NOT NULL,
+ for_date date not null,
  num_seats INT NOT NULL,
  coach_type varchar(10) NOT NULL,
  primary key (pnr),
- foreign key (train_num) references train (train_num)
+ foreign key (train_num, for_date) references schedule(train_num, running_on)
 
 );
  create table passenger(
@@ -34,4 +27,13 @@ create table ticket(
   foreign key (pnr) references ticket(pnr)
  );
 
+ create table curr_avail_coach (
+  train_num int not null,
+  coach_num int not null,
+  coach_type varchar(10) not null,
+  running_on date not null,
+  avail_seat int not null,
+  primary key (train_num, coach_type, running_on),
+  foreign key (train_num,running_on) references schedule (train_num, running_on)
+ );
 
