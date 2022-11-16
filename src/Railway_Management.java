62d155f7eb2373b45b3a4c4d93ac2971a383214b
KartikayIITRpr <92101887+KartikayIITRpr@users.noreply.github.com>
2022-11-16 18:23:33 +0530

    Add files via upload

--- src/Railway_Management.java
+++ src/Railway_Management.java
@@ -5,7 +5,6 @@ import java.util.*;
 
 public class Railway_Management  {
 	
-	
     public static void main(String[] args) throws Exception {
         // Class.forName("org.postgresql.Driver");
         String url = "jdbc:postgresql://localhost:5432/Railway_Management", username = "postgres", password = "Kart1923@1";
@@ -18,146 +17,12 @@ public class Railway_Management  {
         }
         try {
             Statement st = con.createStatement();
-            try {
-            	String drop_file = "src/drop.sql";
-            	String[] drop = (new String(Files.readAllBytes(Paths.get(drop_file)))).split(";");
-                for (String i: drop) {
-                    st.execute(i);
-                }
-            }
-            catch (Exception e) {
-                System.out.println("drop Exception");
-            }
-            try {
-            	String create_file = "src/create.sql";
-                String[] create = (new String(Files.readAllBytes(Paths.get(create_file)))).split(";");
-                for (String i: create) {
-                    st.execute(i);
-                }
-            }
-            catch (Exception e) {
-                System.out.println("Create Exception");
-                System.out.println(e.getMessage());
-            }
-            try {
-            	String stored_file = "src/stored_pro.sql";
-            	String stored_pro = new String(Files.readAllBytes(Paths.get(stored_file)));
-                st.execute(stored_pro);
-            }
-            catch (Exception e) {
-                System.out.println("Stored pro Exception");
-                System.out.println(e.getMessage());
-            }
-            try {
-            	String create_file = "src/triggers.sql";
-                String[] create = (new String(Files.readAllBytes(Paths.get(create_file)))).split(";");
-                for (String i: create) {
-                	st.execute(i);
-                }
-            }
-            catch (Exception e) {
-                System.out.println("Trigger Exception");
-                System.out.println(e.getMessage());
-            }
-            
-            try {
-            	File schedule_file = new File("asset/trainschedule.txt");
-            	Scanner sc = new Scanner(schedule_file);
-            	while (sc.hasNextLine()) {
-            		String[] schedule = (sc.nextLine()).split("\\s+");
-            		String query = "insert into schedule values("+schedule[0]+",\'"+ schedule[1]+ "\',"+ schedule[2] + "," + schedule[3]+ ");";
-//            		System.out.println(query);
-            		st.execute(query);
-            	}
-            	sc.close();
-            }
-            catch (Exception e) {
-                System.out.println("Schedule Exception");
-                System.out.println(e.getMessage());
-            }
-            
-            try {
-            	File booking_file = new File ("asset/bookings.txt");
-            	Scanner sc = new Scanner(booking_file);
-            	while (sc.hasNextLine()) {
-            		String[] booking = (sc.nextLine()).split("[,]?\\s+");
-            		int num = Integer.parseInt(booking[0]);
-            		ArrayList<String> names = new ArrayList<String>();
-            		for (int i = 1; i<=num; i++) {
-            			names.add(booking[i]);
-            		}
-            		String train_num = booking[num+1];
-            		String date = booking[num+2];
-            		String type = booking[num+3];
-            		String avail_seats_query = "Select get_avail_seats("+train_num+", \'"+date+"\', \'"+type+"\');";
-//            		System.out.println(avail_seats);
-            		ResultSet rs = st.executeQuery(avail_seats_query);
-            		rs.next();
-            		int avail_seats = rs.getInt(1);
-            		if (avail_seats <num) {
-            			System.out.println("Seats not available.");
-            		}
-            		else {
-            			if (type == "AC") {
-            				String query = "select update_curr_ac ("+train_num + ", \'"+ date + "\', "+ String.valueOf(num)+ ");";
-            				ResultSet rsq = st.executeQuery(query);
-            				rsq.next();
-            				String[] ret = ((rsq.getString(1)).replaceAll("[()]", "")).split(",\\s*");
-            				int remain_seat = Integer.parseInt(ret[0]), curr_coach = Integer.parseInt(ret[1]);
-            				String get_tick_num = "select get_ticket("+train_num+", \'"+ date + "\', "+ String.valueOf(num)+ ", \'"+ type + "\');";
-            				rsq = st.executeQuery(get_tick_num);
-            				rsq.next();
-            				int pnr = rsq.getInt(1);
-            				int num_booked = 0;
-            				
-            				while (num_booked < num) {
-            					if (remain_seat == 0) {
-            						remain_seat = 18;
-            						curr_coach++;
-            					}
-            					int x = 18-remain_seat+1;
-            					String ins_pass = "insert into passenger values("+ String.valueOf(pnr)+ ", " + String.valueOf(curr_coach)+ ", " + String.valueOf(x) + ", '" + get_berth_type_ac(x) + "', '"+ names.get(num_booked) + "');";
-            					st.execute(ins_pass);
-            					num_booked++;
-            					remain_seat--;
-            					
-            				}
-            			}
-            			else {
-            				String query = "select update_curr_sl ("+train_num + ", \'"+ date + "\', "+ String.valueOf(num)+ ");";
-            				ResultSet rsq = st.executeQuery(query);
-            				rsq.next();
-            				String[] ret = ((rsq.getString(1)).replaceAll("[()]", "")).split(",\\s*");
-            				int remain_seat = Integer.parseInt(ret[0]), curr_coach = Integer.parseInt(ret[1]);
-            				String get_tick_num = "select get_ticket("+train_num+", \'"+ date + "\', "+ String.valueOf(num)+ ", \'"+ type + "\');";
-            				rsq = st.executeQuery(get_tick_num);
-            				rsq.next();
-            				int pnr = rsq.getInt(1);
-            				int num_booked = 0;
-            				
-            				while (num_booked < num) {
-            					if (remain_seat == 0) {
-            						remain_seat = 18;
-            						curr_coach++;
-            					}
-            					int x = 24-remain_seat+1;
-            					String ins_pass = "insert into passenger values("+ String.valueOf(pnr)+ ", " + String.valueOf(curr_coach)+ ", " + String.valueOf(x) + ", '" + get_berth_type_sl(x) + "', '"+ names.get(num_booked) + "');";
-            					st.execute(ins_pass);
-            					num_booked++;
-            					remain_seat--;
-            					
-            				}
-            			}
-            		}
-            		
-            		
-            	}
-            	sc.close();
-            }
-            catch (Exception e) {
-                System.out.println("Booking Exception");
-                System.out.println(e.getMessage());
-            }
+            drop_table(st);
+            create_table(st);
+            create_stored_pro(st);
+            create_trigger(st);
+            schedule_train(st);
+            book_ticket(st);
             
             st.close();
         }
@@ -209,6 +74,150 @@ public class Railway_Management  {
     	default:
     		return "SU";
     	}
+    }
+    
+    static void drop_table (Statement st) {
+    	try {
+        	String drop_file = "src/drop.sql";
+        	String drop = new String(Files.readAllBytes(Paths.get(drop_file)));
+            st.execute(drop);
+        }
+        catch (Exception e) {
+            System.out.println("drop Exception");
+        }
+    }
+    
+    static void create_table (Statement st) {
+    	try {
+        	String create_file = "src/create.sql";
+            String create = new String(Files.readAllBytes(Paths.get(create_file)));
+            st.execute(create);
+        }
+        catch (Exception e) {
+            System.out.println("Create Exception");
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    static void create_stored_pro ( Statement st) {
+    	try {
+        	String stored_file = "src/stored_pro.sql";
+        	String stored_pro = new String(Files.readAllBytes(Paths.get(stored_file)));
+            st.execute(stored_pro);
+        }
+        catch (Exception e) {
+            System.out.println("Stored pro Exception");
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    static void create_trigger (Statement st) {
+    	try {
+        	String trigger_file = "src/triggers.sql";
+            String trigger = new String(Files.readAllBytes(Paths.get(trigger_file)));
+            
+            st.execute(trigger);
+        }
+        catch (Exception e) {
+            System.out.println("Trigger Exception");
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    static void schedule_train (Statement st) {
+    	try {
+        	File schedule_file = new File("asset/trainschedule.txt");
+        	Scanner sc = new Scanner(schedule_file);
+        	while (sc.hasNextLine()) {
+        		String[] schedule = (sc.nextLine()).split("\\s+");
+        		String query = "insert into schedule values("+schedule[0]+",\'"+ schedule[1]+ "\',"+ schedule[2] + "," + schedule[3]+ ");";
+//        		System.out.println(query);
+        		st.execute(query);
+        	}
+        	sc.close();
+        }
+        catch (Exception e) {
+            System.out.println("Schedule Exception");
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    static void book_ticket (Statement st) {
+    	try {
+        	File booking_file = new File ("asset/bookings.txt");
+        	Scanner sc = new Scanner(booking_file);
+        	while (sc.hasNextLine()) {
+        		String[] booking = (sc.nextLine()).split("[,]?\\s+");
+        		int num = Integer.parseInt(booking[0]);
+        		ArrayList<String> names = new ArrayList<String>();
+        		for (int i = 1; i<=num; i++) {
+        			names.add(booking[i]);
+        		}
+        		String train_num = booking[num+1];
+        		String date = booking[num+2];
+        		String type = booking[num+3];
+        		String avail_seats_query = "Select get_avail_seats("+train_num+", \'"+date+"\', \'"+type+"\', " + String.valueOf(num) + ");";
+//        		System.out.println(avail_seats);
+        		ResultSet rs = st.executeQuery(avail_seats_query);
+        		rs.next();
+				String[] ret = ((rs.getString(1)).replaceAll("[()]", "")).split(",\\s*");
+				int remain_seat = Integer.parseInt(ret[0]), curr_coach = Integer.parseInt(ret[1]);
+				if (remain_seat >0) {
+					if (type == "AC") {
+        				ResultSet rsq ;
+        				String get_tick_num = "select get_ticket("+train_num+", \'"+ date + "\', "+ String.valueOf(num)+ ", \'"+ type + "\');";
+        				rsq = st.executeQuery(get_tick_num);
+        				rsq.next();
+        				int pnr = rsq.getInt(1);
+        				int num_booked = 0;
+        				
+        				while (num_booked < num) {
+        					if (remain_seat == 0) {
+        						remain_seat = 18;
+        						curr_coach++;
+        					}
+        					int x = 18-remain_seat+1;
+        					String ins_pass = "insert into passenger values("+ String.valueOf(pnr)+ ", " + String.valueOf(curr_coach)+ ", " + String.valueOf(x) + ", '" + get_berth_type_ac(x) + "', '"+ names.get(num_booked) + "');";
+        					st.execute(ins_pass);
+        					num_booked++;
+        					remain_seat--;
+        					
+        				}
+        			}
+        			else {
+        				ResultSet rsq;
+        				String get_tick_num = "select get_ticket("+train_num+", \'"+ date + "\', "+ String.valueOf(num)+ ", \'"+ type + "\');";
+        				rsq = st.executeQuery(get_tick_num);
+        				rsq.next();
+        				int pnr = rsq.getInt(1);
+        				int num_booked = 0;
+        				
+        				while (num_booked < num) {
+        					if (remain_seat == 0) {
+        						remain_seat = 18;
+        						curr_coach++;
+        					}
+        					int x = 24-remain_seat+1;
+        					String ins_pass = "insert into passenger values("+ String.valueOf(pnr)+ ", " + String.valueOf(curr_coach)+ ", " + String.valueOf(x) + ", '" + get_berth_type_sl(x) + "', '"+ names.get(num_booked) + "');";
+        					st.execute(ins_pass);
+        					num_booked++;
+        					remain_seat--;
+        					
+        				}
+        			}
+				}
+				else {
+					System.out.println("Seats not available");
+				}
+        		
+        		
+        	}
+        	sc.close();
+        }
+        catch (Exception e) {
+            System.out.println("Booking Exception");
+            System.out.println(e.getMessage());
+        }
     }
     
 }
--- src/stored_pro.sql
+++ src/stored_pro.sql
@@ -1,62 +1,5 @@
-create or replace function get_avail_seats (train_num int, running_on date, coach_type varchar(10))
-returns int
-as $$
-declare
-ans int;
-curr_coach int;
-tot_ac int;
-tot_sl int;
-begin
-	select num_ac,num_sl into tot_ac, tot_sl
-	from schedule s
-	where s.train_num = $1 and s.running_on = $2;
-	
-	if coach_type = 'AC' then 
-		select c.avail_seat, c.coach_num into ans,curr_coach
-		from curr_avail_ac c
-		where c.train_num = $1 and c.running_on = $2;
-	
-	elsif coach_type = 'SL' then
-		select c.avail_seat, c.coach_num into ans,curr_coach
-		from curr_avail_sl c
-		where c.train_num = $1 and c.running_on = $2 ;	
-	end if;
-	
-	
-	if not found then
-		raise exception 'Train not scheduled for the day';
-		return -1;
-	end if;
-	
-	if coach_type = 'AC' then
-		ans := ans + (tot_ac - curr_coach)*18;
-	else
-		ans := ans + (tot_ac + tot_sl - curr_coach)*24;
-	end if;
-	
-	return ans;
-	
-end;
-$$ language plpgsql;
-
-create or replace function insert_avail() 
-returns trigger
-as $$
-begin
-	if new.num_ac >0 then
-		insert into curr_avail_ac values(new.train_num, 1, new.running_on, 18);
-	end if;
-	
-	if new.num_sl >0 then
-		insert into curr_avail_sl values(new.train_num, new.num_ac+1, new.running_on, 24);
-	end if;
-	
-	return new;
-end;
-$$ language plpgsql;
-
 create or replace function update_curr_ac (train_num int, running_on date, tot_num int )
-returns record
+returns void
 as $$
 declare
 curr_coach int;
@@ -84,13 +27,11 @@ begin
 	end if;
 	
 	select curr_coach, remain_seats into ret_rec;
-	
-	return (remain_seats, curr_coach);
 end;
 $$ language plpgsql;
 
 create or replace function update_curr_sl (train_num int, running_on date, tot_num int )
-returns record
+returns void
 as $$
 declare
 curr_coach int;
@@ -118,8 +59,85 @@ begin
 	end if;
 	
 	select curr_coach, remain_seats into ret_rec;
+end;
+$$ language plpgsql;
+
+create or replace function get_avail_seats (train_num int, running_on date, coach_type varchar(10), tot_num int)
+returns record
+as $$
+declare
+ans int;
+remain_seats int;
+curr_coach int;
+tot_ac int;
+tot_sl int;
+ret_rec record;
+temp record;
+begin
+	select num_ac,num_sl into tot_ac, tot_sl
+	from schedule s
+	where s.train_num = $1 and s.running_on = $2;
 	
-	return (remain_seats, curr_coach);
+	
+	if coach_type = 'AC' then 
+		select * into temp
+		from curr_avail_ac c
+		where c.train_num = $1 and c.running_on = $2 for update;
+		select c.avail_seat, c.coach_num into ans,curr_coach
+		from curr_avail_ac c
+		where c.train_num = $1 and c.running_on = $2;
+	
+	elsif coach_type = 'SL' then
+		select * into temp from curr_avail_sl c where c.train_num = $1 and c.running_on = $2 for update;
+		select c.avail_seat, c.coach_num into ans,curr_coach
+		from curr_avail_sl c
+		where c.train_num = $1 and c.running_on = $2 ;	
+	end if;
+	
+	
+	if not found then
+		raise exception 'Train not scheduled for the day';
+		return (-1,-1);
+	end if;
+	
+	if coach_type = 'AC' then
+		remain_seats := ans;
+		ans := ans + (tot_ac - curr_coach)*18;
+		if ans<tot_num then
+			return (-1,-1);
+		else
+			perform update_curr_ac($1,$2,$4);
+			select remain_seats, curr_coach into ret_rec;
+			return ret_rec;
+		end if;
+	else
+		remain_seats := ans;
+		ans := ans + (tot_ac + tot_sl - curr_coach)*24;
+		if ans<tot_num then
+			return (-1,-1);
+		else
+			perform update_curr_sl($1,$2,$4);
+			select remain_seats, curr_coach into ret_rec;
+			return ret_rec;
+		end if;
+	end if;
+	
+end;
+$$ language plpgsql;
+
+create or replace function insert_avail() 
+returns trigger
+as $$
+begin
+	if new.num_ac >0 then
+		insert into curr_avail_ac values(new.train_num, 1, new.running_on, 18);
+	end if;
+	
+	if new.num_sl >0 then
+		insert into curr_avail_sl values(new.train_num, new.num_ac+1, new.running_on, 24);
+	end if;
+	
+	return new;
 end;
 $$ language plpgsql;
 
